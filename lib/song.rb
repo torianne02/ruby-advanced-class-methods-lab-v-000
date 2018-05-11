b6379908a5270f697e69d528b1fef5ef966b0567
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-11 17:36:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,36 @@ class Song
     self.class.all << self
   end
 
+  def Song.create
+    new_song = Song.new
+    Song.all << new_song
+    return new_song
+  end
+
+  def Song.new_by_name(name)
+    @name = name
+  end
+
+  def Song.create_by_name(name)
+  end
+
+  def Song.find_by_name(name)
+  end
+
+  def Song.find_or_create_by_name(name)
+  end
+
+  def Song.alphabetical
+    #Song.all.sort_by
+  end
+
+  def Song.new_from_filename
+  end
+
+  def Song.create_from_filename
+  end
+
+  def Song.destroy_all
+    Song.all.clear
+  end
 end
