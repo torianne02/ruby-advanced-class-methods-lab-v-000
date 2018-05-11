cb5dfe4cd15bba7958a54e203d27c7569fd04ad9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-11 18:27:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,15 +16,16 @@ class Song
     new_song
   end
 
-  def self.new_by_name(song_name)
+  def self.new_by_name(song_name, artist_name = nil)
     new_song = self.new
     new_song.name = song_name
     new_song
   end
 
-  def self.create_by_name(song_name)
+  def self.create_by_name(song_name, artist_name = nil)
     new_song = self.new
     new_song.name = song_name
+    new_song.artist_name = artist_name
     @@all << new_song
     new_song
   end
